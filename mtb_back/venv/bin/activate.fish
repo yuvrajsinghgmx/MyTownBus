e139f898eabe88327c44e9347549773f63fc5cc1
Yuvraj Singh <qomfortzone@gmail.com>
2024-11-29 22:04:21 +0530

    venv update

--- mtb_back/venv/bin/activate.fish
+++ mtb_back/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/yuvrajsingh/Codes/My Town Bus/mtb/venv'
+set -gx VIRTUAL_ENV '/home/yuvrajsingh/Codes/My Town Bus/mtb_back/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
